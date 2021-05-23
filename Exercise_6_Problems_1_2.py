1af8d8cbb0b40090ffe8eba4a3f8157dfc35be32
a5719083 <>
2021-05-23 14:42:26 +0000

    Assign to tmin_nodata_count

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -43,7 +43,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 
 tmin_nodata_count = None
 #YOUR CODE HERE 3
-tavg_nodata_count=data.iloc[:,8].isnull().sum()
+tmin_nodata_count=data.iloc[:,8].isnull().sum()
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Number of no-data values in column "TMIN":', tmin_nodata_count)
